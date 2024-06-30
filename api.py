488a6f5fd1301b4aa0634dfb37a0e4539b86556f
Jenbigelow <jennifer.o.bigelow@gmail.com>
2024-06-29 19:34:32 -0500

    parsed API from JSON for majority of relevant data regarding yarn

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from pprint import pformat, pprint
 import requests
 import os
 import json
+import re
 
 app = Flask(__name__)
 
@@ -19,13 +20,10 @@ url ="https://api.ravelry.com/stash/search.json?query"
 
 res = requests.get(url, params = payload, auth = auth_params)
 
-# print(res.json())
 
 stashes = res.json()["stashes"]
-# print(stashes)
 
 yarn_ID = stashes[1]["id"]
-# print(yarn_ID)
 
 total_yarn_data = {}
 for yarn in stashes:
@@ -33,7 +31,16 @@ for yarn in stashes:
     rav_username = yarn["user"]["username"]
     user_url = f"https://api.ravelry.com//people/{rav_username}/stash/{yarn_ID}.json"
     yarn_data = requests.get(user_url, auth = auth_params)
-    notes_html = yarn_data.json()["stash"]["notes_html"]
-    # print(notes_html)
+    yarn_JSON_data = yarn_data.json()["stash"]
+    notes_html = yarn_JSON_data["notes"]
+    # pprint(yarn_JSON_data) 
     if "$" in notes_html:
-        print(notes_html)
+        yarn_price = re.search(r"[0-9]+", notes_html).group()
+        yarn_price = int(yarn_price)
+        yarn_name = yarn_JSON_data["name"]
+        yarn_weight = yarn_JSON_data["yarn_weight_name"]
+        yarn_skeins = yarn_JSON_data["packs"][0]["skeins"]
+        # yarn_company = yarn_JSON_data["yarn"]["yarn_company"]["name"]
+        dye_lot = yarn_JSON_data["dye_lot"]
+        yarn_photo = yarn_JSON_data["photos"][0]["medium2_url"]
+        print(yarn_name, yarn_price, yarn_weight, yarn_skeins, dye_lot, yarn_photo)
