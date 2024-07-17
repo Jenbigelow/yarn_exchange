9abd37678a3fed5756fee84f10f61189cee65c35
Jenbigelow <jennifer.o.bigelow@gmail.com>
2024-07-16 22:32:05 -0500

    individual yarn pages achieved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import {
-  createBrowserRouter,
-  RouterProvider,
   Routes,
   Route,
   Link,
-  Outlet
+  Outlet,
+  useParams
 } from "react-router-dom";
 function Layout() {
   return(
@@ -22,6 +21,37 @@ function Layout() {
   )
 }
 
+
+  
+function App() {
+  const [yarns, setYarns] = useState({});
+  useEffect(() => {
+    fetch("/api/yarns")
+      .then((response) => response.json())
+      .then((yarnData) => {
+        setYarns(yarnData.yarns);
+      });
+  }, []);
+
+
+  return(
+  <> 
+  <div>
+  <Routes>
+  <Route path = "" element = {<Layout />}/><Route/>
+  <Route path="About" element={<About />} /><Route/>
+  <Route path="Yarns" element ={<Yarns yarns = {yarns}/>}/> <Route/>
+    <Route path="/yarns/:yarnId" element={<Yarn/>} />
+ 
+  </Routes>
+  </div>
+  </>
+  )
+}
+
+
+export default App
+
 function About(){
 
   return(
@@ -31,69 +61,79 @@ function About(){
   )
 }
 
-function Yarns() {
-  const [yarns, setYarns] = useState(null);
-  useEffect(() => {
-    fetch("/api/yarns")
-      .then((response) => response.json())
-      .then((yarnData) => {
-        setYarns(yarnData.yarns);
-      });
-  }, []);
+function Yarns(props) {
+const { yarns } = props
+const yarnCards = [];
+// if yarn is null
+for (const yarn of Object.values(yarns)) {
+  const yarnCard = (
+    <YarnCard
+      key={yarn.yarn_id}
+      yarnId={yarn.yarn_id}
+      yarnName={yarn.yarn_name}
+      yarnPhoto={yarn.yarn_photo}
+      yarnPrice={yarn.yarn_price}
+    />
+  );
+
+  yarnCards.push(yarnCard);
+}
 
   return (
-    
     <>
     <h1>Yarn</h1>
     <div>
-
+  
       </div>
     {yarns === null ? (
       <div>Loading...</div>
-    ) : (
-      yarns.map((yarn) => <div key={yarn.yarn_id}>{yarn.yarn_name}${yarn.yarn_price}
-      <div>
-      <img src={`${yarn.yarn_photo}`}/>
-      </div>
-      <div>
-  <Link to={`/${yarn.yarn_name}`}>{yarn.yarn_name}</Link>
-  </div>
-
-    </div>
-
-
-
-
+    ) :(<div>{yarnCards}</div>)}
+    <Outlet />
+    </>
+    
   )
-    )}
-  </>
-  );
-
 }
 
-function YarnPage(props){
-  const yarn_name = props;
+const Yarn = () => {
+// const params = useParams()
+// const yarnId = params.yarnId
+  const {yarnId} = useParams();
+
 return (
-  <p>Hello {props.yarn_name}!</p>
+  <>
+  <p>Hello {yarnId}!</p>
+  <div>
+      <Link to="/yarns">Back to Yarns</Link>
+  </div>
+  </>
+
 )
 }
-  
-function App() {
 
+function YarnCard(props){
+  const {yarnId, yarnName, yarnPrice, yarnPhoto} = props
+  console.log(props)
   return(
-  <>
-  <div>
-  <Routes>
-  <Route path = "" element = {<Layout />}/><Route/>
-  <Route path="About" element={<About />} /><Route/>
-  <Route path="Yarns" element={<Yarns />} />
-  <Route path="YarnPage" element={<YarnPage />} />
-  <Route/>
-  </Routes>
+
+    <div>{yarnName}${yarnPrice}
+    <div>
+    <img src={`${yarnPhoto}`}/>
+    </div>
+    <div>
+<Link to={`/yarns/${yarnName}`}> {yarnName}</Link>
+</div>
+
   </div>
-  </>
-  )
+
+)
 }
 
-
-export default App
+// function YarnName(props){
+//   const { yarns } = props
+//   for (const yarn of Object.values(yarns)) {
+//     let yarn_name = yarn.yarn_name
+//        return(<Route exact path={`/${yarn_name}`} element={<YarnPage/>}/>)
+      
+  
+//   }
+//   }
